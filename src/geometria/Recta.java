0c2d00fb7a8be28a04b8aaa89808404fc8e02828
Iván <ivanantonsagastizabal@gmail.com>
2021-05-17 09:54:30 +0200

    Añadidos los constructores de la recta

--- src/geometria/Recta.java
+++ src/geometria/Recta.java
@@ -1,9 +1,27 @@
 package geometria;
 
 public class Recta {
+	
 	//Atributos
-
+	private Punto punto;
+	private double pendiente;
+	
 	//Constructores
+	public Recta(Punto punto, double pendiente) {
+		this.punto = punto;
+		this.pendiente = pendiente;
+	}
+	
+	public Recta(Punto puntoInicial, Punto puntoFinal) {
+		this.punto = puntoInicial;
+		this.pendiente = (puntoFinal.getCoordenadaY() - puntoInicial.getCoordenadaY()) / (puntoFinal.getCoordenadaX() - puntoInicial.getCoordenadaX());
+	}
+	
+	public Recta() {
+		this.punto = new Punto();
+		Punto puntoSecundario = new Punto();
+		this.pendiente = (punto.getCoordenadaY() - puntoSecundario.getCoordenadaY()) / (punto.getCoordenadaX() - puntoSecundario.getCoordenadaX());
+	}
 
 	//Getters
 
