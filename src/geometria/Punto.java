77fecf7ba72df1e61ce27a73753cd55939cc2d5b
Iván <ivanantonsagastizabal@gmail.com>
2021-05-17 09:38:21 +0200

    Finalizada la clase punto

--- src/geometria/Punto.java
+++ src/geometria/Punto.java
@@ -1,13 +1,63 @@
 package geometria;
 
 public class Punto {
+	
 	//Atributos
-
+	private int coordenadaX;
+	private int coordenadaY;
+	
 	//Constructores
+	public Punto(int coordenadaX, int coordenadaY) {
+		this.coordenadaX = coordenadaX;
+		this.coordenadaY = coordenadaY;
+	}
+	
+	public Punto() {
+		this.coordenadaX = (int) (Math.random()*10);
+		this.coordenadaY = (int) (Math.random()*10);
+	}
 
 	//Getters
+		public int getCoordenadaX() {
+		return coordenadaX;
+	}
 
+	public int getCoordenadaY() {
+		return coordenadaY;
+	}
+	
 	//Setters
+	public void setCoordenadaX(int coordenadaX) {
+		this.coordenadaX = coordenadaX;
+	}
 
+	public void setCoordenadaY(int coordenadaY) {
+		this.coordenadaY = coordenadaY;
+	}
+
+	
 	//Metodos
+	@Override
+	public boolean equals(Object obj) {
+		return this.hashCode() == obj.hashCode();
+	}
+
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return "Punto [coordenadaX=" + coordenadaX + ", coordenadaY=" + coordenadaY + "]";
+	}
+	
+	public double calcularDistancia(Punto puntoObjetivo) {
+		return Math.sqrt((Math.pow(this.coordenadaX - puntoObjetivo.getCoordenadaX(), 2) + Math.pow(this.coordenadaY - puntoObjetivo.getCoordenadaY(), 2)));
+	}
+	
+	public Punto puntoMedioEntreDosPuntos(Punto segundoPunto) {
+		Punto p = new Punto((this.getCoordenadaX() + segundoPunto.getCoordenadaX())/2, (this.getCoordenadaY() + segundoPunto.getCoordenadaY())/2);
+		return p;
+	}
 }
