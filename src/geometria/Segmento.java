c037c5ec2c05afa5816f8a744a8c9c0fa9e155aa
Iván <ivanantonsagastizabal@gmail.com>
2021-05-21 12:00:05 +0200

    Añadido constructor del segmento

--- src/geometria/Segmento.java
+++ src/geometria/Segmento.java
@@ -2,9 +2,19 @@ package geometria;
 
 public class Segmento {
 	//Atributos
-
+	Punto puntoInicial;
+	Punto puntoFinal;
+	Recta rectaFormada;
 	//Constructores
-
+	public Segmento(Punto puntoInicial, Punto puntoFinal) {
+		if (puntoInicial != puntoFinal) {
+			this.puntoInicial = puntoInicial;
+			this.puntoFinal = puntoFinal;
+			this.rectaFormada = new Recta(puntoInicial, puntoFinal);
+		}
+		
+	}
+	
 	//Getters
 
 	//Setters
