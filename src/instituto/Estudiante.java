1fae4f1a7a12a169165635834feffbcc38537acf
ivan anton <ivanantonsagastizabal@gmail.com>
2021-05-22 18:41:16 +0200

    Clase Estudiante 1.1
    Modificados los metodos con Override
    generado correctamente el setter setAsignaturasMatriculadas(String[] s)

--- src/instituto/Estudiante.java
+++ src/instituto/Estudiante.java
@@ -6,18 +6,37 @@ public class Estudiante extends Persona {
 	private String[] asignaturasMatriculadas;
 	
 	//Constructores
-	public Estudiante(String nombre, int edad, char sexo) {
+	public Estudiante(String nombre, int edad, char sexo, String[] asignaturasMatriculadas) {
 		super(nombre, edad, sexo);
-		// TODO Auto-generated constructor stub
+		if (Asignatura.asignaturaDisponible(asignaturasMatriculadas)) {
+			this.asignaturasMatriculadas = asignaturasMatriculadas;
+		}
 	}
+	
 	//Getters
+	public synchronized String[] getAsignaturasMatriculadas() {
+		return asignaturasMatriculadas;
+	}
 
 	//Setters
-
+	public synchronized void setAsignaturasMatriculadas(String[] asignaturasMatriculadas) {
+		if (Asignatura.asignaturaDisponible(asignaturasMatriculadas)) {
+			this.asignaturasMatriculadas = asignaturasMatriculadas;
+		}
+		else {
+			throw new RuntimeException("Una o varias asignaturas no están dadas de alta en el centro");
+		}
+	}
+	
 	//Metodos
 	@Override
 	public boolean personaPuedeAsistir() {
-		// TODO Auto-generated method stub
-		return false;
+		if (Math.random()*100 <= 25) {
+			return false;
+		}
+		else {
+			return true;
+		}
 	}
+
 }
